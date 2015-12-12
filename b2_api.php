ebbf8e1453de52093a6bd0b674504b2bec20ad1d
Dan Rovito <danrovito@gmail.com>
2015-12-12 16:46:59 -0500

    add bucket

--- b2_api.php
+++ b2_api.php
@@ -1,53 +1,92 @@
 <?php
 
-/**
-* Backblaze B2 API Wrapper
-* 
-* @author Dan Rovito
-* @copyright OhioValleyPHP.com
-* @version dev-master
-*
-*/
+    /**
+     * Backblaze B2 API Wrapper
+     *
+     * @author Dan Rovito
+     * @copyright OhioValleyPHP.com
+     * @version dev-master
+     *
+     */
+    class b2_api
+    {
 
-class b2_api
-{
+        //Account Authorization
+        public function b2_authorize_account($acct_id, $app_key)
+        {
+            $account_id      = $acct_id;
+            $application_key = $app_key;
+            $credentials     = base64_encode($account_id . ":" . $application_key);
+            $url             = "https://api.backblaze.com/b2api/v1/b2_authorize_account";
 
-  //Account Authorization
-  public function b2_authorize_account($acctt_id, $app_key)
-  {
-    $account_id = $acctt_id; 
-    $application_key = $app_key; 
-    $credentials = base64_encode($account_id . ":" . $application_key);
-    $url = "https://api.backblaze.com/b2api/v1/b2_authorize_account";
+            $session = curl_init($url);
 
-    $session = curl_init($url);
+            // Add headers
+            $headers   = array();
+            $headers[] = "Accept: application/json";
+            $headers[] = "Authorization: Basic " . $credentials;
+            curl_setopt($session, CURLOPT_HTTPHEADER, $headers);  // Add headers
 
-    // Add headers
-    $headers = array();
-    $headers[] = "Accept: application/json";
-    $headers[] = "Authorization: Basic " . $credentials;
-    curl_setopt($session, CURLOPT_HTTPHEADER, $headers);  // Add headers
+            curl_setopt($session, CURLOPT_HTTPGET, true);  // HTTP GET
+            curl_setopt($session, CURLOPT_RETURNTRANSFER, true); // Receive server response
 
-    curl_setopt($session, CURLOPT_HTTPGET, true);  // HTTP GET
-    curl_setopt($session, CURLOPT_RETURNTRANSFER, true); // Receive server response
+            $http_result = curl_exec($session); //results
+            $error       = curl_error($session); //Error return
+            $http_code   = curl_getinfo($session, CURLINFO_HTTP_CODE); //Result type: 200, 404, 500, etc.
 
-    $http_result = curl_exec($session); //results
-    $error       = curl_error($session); //Error return
-    $http_code   = curl_getinfo($session, CURLINFO_HTTP_CODE); //Result type: 200, 404, 500, etc.
+            curl_close($session);
 
-    curl_close ($session);
+            //Print result code if it doesn't equal 200
+            if ($http_code != 200)
+            {
+                return print $http_code;
+            } else
+            {
+                //Return results
+                return $http_result;
+            }
 
-    //Print result code if it doesn't equal 200
-    if ($http_code != 200) {
-      return print $http_code;
-    } 
-    else {
-      //Return results 
-      return $http_result;
+
+        }
+
+        //Create Bucket
+        public function b2_create_bucket($acct_id, $api_auth_url, $api_auth_token, $api_bucket_name, $bucket_type)
+        {
+            $account_id  = $acct_id; // Obtained from your B2 account page
+            $api_url     = $api_auth_url; // From b2_authorize_account call
+            $auth_token  = $api_auth_token; // From b2_authorize_account call
+            $bucket_name = $api_bucket_name; // 6 char min, 50 char max: letters, digits, - and _
+            $bucket_type = $bucket_type; // Either allPublic or allPrivate
+
+            $session = curl_init($api_url . "/b2api/v1/b2_create_bucket");
+
+            // Add post fields
+            $data        = array("accountId" => $account_id, "bucketName" => $bucket_name, "bucketType" => $bucket_type);
+            $post_fields = json_encode($data);
+            curl_setopt($session, CURLOPT_POSTFIELDS, $post_fields);
+
+            // Add headers
+            $headers   = array();
+            $headers[] = "Authorization: " . $auth_token;
+            curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
+
+            curl_setopt($session, CURLOPT_POST, true); // HTTP POST
+            curl_setopt($session, CURLOPT_RETURNTRANSFER, true);  // Receive server response
+
+            $http_result = curl_exec($session); //results
+            $error       = curl_error($session); //Error return
+            $http_code   = curl_getinfo($session, CURLINFO_HTTP_CODE); //Result type: 200, 404, 500, etc.
+
+            curl_close($session); // Clean up
+
+            //Print result code if it doesn't equal 200
+            if ($http_code != 200)
+            {
+                return print $http_code;
+            } else
+            {
+                //Return results
+                return $http_result;
+            }
+        }
     }
-
-
-  }
-}
-
-?>
