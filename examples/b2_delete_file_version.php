188eb9f737034635d2197fdc843ee69ebb234a6f
Aidhan Dossel <me@aidhan.net>
2016-02-19 00:19:53 +0800

    Elaborate comments

--- examples/b2_delete_file_version.php
+++ examples/b2_delete_file_version.php
@@ -3,8 +3,8 @@ require "../b2_api.php"; // Include the API wrapper
 
 $account_id = ""; // Account ID, obtained from your B2 bucket page
 $app_key    = ""; // Application ID, obtained from your B2 bucket page
-$file_id    = ""; // The ID of the file
-$file_name  = ""; // The name of the file
+$file_id    = ""; // The ID of the file you want to delete
+$file_name  = ""; // The file name of the file you want to delete
 
 $b2 = new b2_api; // Create a new instance of b2_api
 $output = $b2->b2_authorize_account($account_id, $app_key); // Runs b2_authorize_account
