46b620ac0fbc38ea37837919043aa138e2ffa22f
Christian Haschek <christian@haschek.at>
2016-07-24 12:24:35 +0200

    auth token provided must be from upload URL

--- examples/b2_upload_file.php
+++ examples/b2_upload_file.php
@@ -9,7 +9,7 @@ $file_path  = ""; // Path to the file you wish to upload
 $b2 = new b2_api; // Create a new instance of b2_api
 $auth = $b2->b2_authorize_account($account_id, $app_key); // Runs b2_authorize_account
 $url = $b2->b2_get_upload_url($auth->apiUrl, $auth->accountId, $auth->authorizationToken, $bucket_id); // Runs b2_get_upload_url
-$output = $b2->b2_upload_file($url->uploadUrl, $auth->authorizationToken, $file_path); // Runs b2_get_upload_url
+$output = $b2->b2_upload_file($url->uploadUrl, $url->authorizationToken, $file_path); // Runs b2_get_upload_url
 
 echo "b2_upload_file\n\n";
 var_dump($output);
